4d9d38df2c4ea197159cd12e4825996f93329266
Niten <niten2@yandex.ru>
2015-11-09 20:46:34 +0700

    t.belongs_to

--- db/migrate/20151109041107_create_answers.rb
+++ db/migrate/20151109041107_create_answers.rb
@@ -1,8 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.text   :content
-      t.integer :question_id
+      t.text       :content
+      t.belongs_to :question
       t.timestamps null: false
     end
   end
