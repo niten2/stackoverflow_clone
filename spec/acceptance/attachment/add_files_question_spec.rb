c78bd85c747b938503b58b38f79cd4cc37696f30
Niten <niten2@yandex.ru>
2015-11-22 16:42:08 +0700

    add test anwers

--- spec/acceptance/attachment/add_files_question_spec.rb
+++ spec/acceptance/attachment/add_files_question_spec.rb
@@ -5,6 +5,7 @@ feature 'Add files to question' do
   given(:user) { create(:user) }
   given(:other_user) { create(:user) }
   given(:question) { create(:question, user: user) }
+  given(:answer) { create(:answer, user: user, question: question) }
   given!(:attachment) { create(:attachment, attachable: question) }
   given(:second_question) { create(:question, user: user) }
 
