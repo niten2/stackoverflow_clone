fabceba5c8d58539b3fd75010a6c5c24052ec1f1
Niten <niten2@yandex.ru>
2015-11-23 14:32:14 +0700

    edit attachment, delete uploads

--- app/controllers/attachment_controller.rb
+++ app/controllers/attachment_controller.rb
@@ -1,8 +1,12 @@
 class AttachmentController < ApplicationController
 
   def destroy
+    # binding.pry
+    # @question = Question.find(params[:question_id])
+    # @answer = Question.find(params[:answer_id])
+
     @attachment = Attachment.find(params[:id])
-    @attachment.destroy
+    @attachment.destroy if @attachment.attachable.user_id == current_user.id
     redirect_to :back
   end
 
--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -1,8 +1,6 @@
 FactoryGirl.define do
   factory :attachment do
-    # file "#{Rails.root}/spec/spec_helper.rb"
     file { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'spec_helper.rb')) }
-    # question
     association :attachable, factory: :question
   end
 
