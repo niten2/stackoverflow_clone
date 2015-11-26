bfbac5f26bbe21f2ce473dcbf23fb540feefc9f0
Niten <niten2@yandex.ru>
2015-11-26 21:43:40 +0700

    refactor view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,11 @@
 //= require jquery
 //= require jquery_ujs
+//= require jquery_nested_form
 //= require jquery.remotipart
 //= require skim
-//= require_tree ./templates
 //= require cocoon
 //= require materialize-sprockets
-//= require jquery_nested_form
+//= require_tree ./templates
 //= require_tree .
 
 
