b86d4eb84be6d5b25d6274603055d9ce2e1cfb13
Niten <niten2@yandex.ru>
2015-11-30 19:12:24 +0700

    add fix bags

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,5 +5,6 @@ describe Comment do
   it { should belong_to :user }
   it { should belong_to :commentable }
   it { should validate_presence_of(:content) }
+  it { should validate_presence_of(:user_id) }
 
 end
