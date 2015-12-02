a2db6a04d380762001eb8590a55588fd6eedc3e4
Niten <niten2@yandex.ru>
2015-12-02 15:48:53 +0700

    clear code

--- spec/support/omniauth_macros.rb
+++ spec/support/omniauth_macros.rb
@@ -5,32 +5,4 @@ module OmniauthMacros
     }.merge(provider == :facebook ? { info: { email: 'test@email.com' }} : {}))
   end
 
-  # def mock_auth_hash
-  #   OmniAuth.config.mock_auth[:twitter] = {
-  #     'provider' => 'twitter',
-  #     'uid' => '123545',
-  #     'user_info' => {
-  #       'name' => 'mockuser',
-  #       'image' => 'mock_user_thumbnail_url'
-  #     },
-  #     'credentials' => {
-  #       'token' => 'mock_token',
-  #       'secret' => 'mock_secret'
-  #     }
-  #   }
-  # end
-
-  # def mock_auth_hash
-  #   OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new(
-  #       provider: 'facebook',
-  #       uid: '123456',
-  #       info: {
-  #           email: 'mockuser@mail.ru',
-  #       },
-  #   )
-
-  #   OmniAuth.config.add_mock(:twitter, {:uid => '12345'})
-  # end
-
-
 end
