1771db1a389a3234f3e6417203aa07652ed9c2b2
Niten <niten2@yandex.ru>
2015-12-11 11:23:43 +0700

    add shared_example

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -3,19 +3,7 @@ require 'rails_helper'
 describe 'Questions API' do
 
   describe 'GET /index' do
-
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        get '/api/v1/questions', format: :json
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        get '/api/v1/questions', format: :json, access_token: '1234'
-        expect(response.status).to eq 401
-      end
-    end
-
+    it_behaves_like "API Authenticable"
     context 'authorized', :lurker do
       let(:access_token) { create(:access_token) }
       let!(:questions) { create_list(:question, 2) }
@@ -31,35 +19,13 @@ describe 'Questions API' do
       it 'returns list of questions' do
         expect(response.body).to have_json_size(2).at_path("questions")
       end
-
-      # %w(id title content created_at updated_at).each do |attr|
-      #   it "question object contains #{attr}" do
-      #     expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("questions/0/#{attr}")
-      #   end
-      # end
-
-      it 'question object contains short_title' do
-        expect(response.body).to be_json_eql(question.title.truncate(10).to_json).at_path("questions/0/short_title")
-      end
-
     end
   end
 
   describe 'GET /show' do
+    it_behaves_like "API Authenticable"
     let(:access_token) { create(:access_token) }
     let(:question) { create(:question) }
-
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        get "/api/v1/questions/#{question.id}", format: :json
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        get "/api/v1/questions/#{question.id}", format: :json, access_token: '1234'
-        expect(response.status).to eq 401
-      end
-    end
 
     context 'authorized', :lurker do
       let!(:answer) { create(:answer, question: question) }
@@ -71,41 +37,11 @@ describe 'Questions API' do
       it 'returns 200 status code' do
         expect(response).to be_success
       end
-
-      %w(id title content created_at updated_at).each do |attr|
-        it "question object contains #{attr}" do
-          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("question/#{attr}")
-        end
-      end
-
-      %w(id content created_at updated_at).each do |attr|
-        it "question object contains comment with #{attr}" do
-          expect(response.body).to be_json_eql(comment_question.send(attr.to_sym).to_json).at_path("question/comments/0/#{attr}")
-        end
-      end
-
-      %w(id file created_at updated_at).each do |attr|
-        it "question object contains attachment with #{attr}" do
-          expect(response.body).to be_json_eql(attachment_question.send(attr.to_sym).to_json).at_path("question/attachments/0/#{attr}")
-        end
-      end
     end
   end
 
   describe 'GET /create' do
-
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        post "/api/v1/questions/", format: :json
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        post "/api/v1/questions/", format: :json, access_token: '1234'
-        expect(response.status).to eq 401
-      end
-    end
-
+    it_behaves_like "API Authenticable"
     context 'authorized' do
       let(:me) { create(:user) }
       let(:access_token) { create(:access_token, resource_owner_id: me.id) }
@@ -132,6 +68,10 @@ describe 'Questions API' do
         end
       end
     end
+  end
+
+  def do_request(options = {})
+    get '/api/v1/profiles/me', { format: :json }.merge(options)
   end
 end
 
